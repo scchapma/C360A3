cf72dec42e066278810cf9be8f0924ab8a61c658
Stephen Chapman <stephen.chapman@krisandbrake.com>
2015-03-22 16:36:27 -0700

    scanned pdf

--- diskget.c
+++ diskget.c
@@ -18,17 +18,17 @@ int main()
 	char *tmp3 = malloc(sizeof(char));
 	char *tmp4 = malloc(sizeof(char));
 
-	int base = 67584;
+	int base = 16896;
 	int i = 0;
 	int j = 0;
 	
 	if ((fp=fopen("disk2.IMA","r")))
 	{
-		fp2 = fopen("file3.tex" , "w");
+		fp2 = fopen("file4.pdf" , "w");
 		
 		int cur = base;
 
-		for (i = 0; i<34; i++){
+		for (i = 0; i<98; i++){
 			fseek(fp,cur,SEEK_SET);
 			for (j = 0; j < 512; j++){
 				fread(tmp1,1,1,fp);
